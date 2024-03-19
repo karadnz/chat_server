b38694fb3e2ef9ecc01b0fe2ddf1dca6c3c7a397
Mustafa Karadeniz <mkaradeniz2003@gmail.com>
2024-03-19 04:09:08 +0300

    updated

--- client_utils.c
+++ client_utils.c
@@ -1,8 +1,20 @@
 #include "server.h"
 
-
-t_client *get_client(t_server *server, struct sockaddr_in client_addr)
+// 
+t_client *new_client(struct sockaddr_in client_addr, int connfd, int uid)
 {
     t_client *client = (t_client *)malloc(sizeof(t_client));
 
+    client->addr = client_addr;
+    client->connfd = connfd;
+    client->uid = uid;
+
+    sprintf(client->name, "%s", client->uid);
 }
+
+
+// t_client *client = (t_client *)malloc(sizeof(t_client));
+// 		client->addr = client_addr;
+// 		client->connfd = connfd;
+// 		client->uid = server->uid++;
+// 		sprintf(client->name, "%d", client->uid);
--- server.c
+++ server.c
@@ -28,12 +28,7 @@ int main(int argc, char **argv)
 			continue;
 		}
 
-		//client settings
-		t_client *client = (t_client *)malloc(sizeof(t_client));
-		client->addr = client_addr;
-		client->connfd = connfd;
-		client->uid = server->uid++;
-		sprintf(client->name, "%d", client->uid);
+		t_client *client = new_client(client_addr, connfd, server->uid++);
 
 		//
 		queue_add(server, client);
